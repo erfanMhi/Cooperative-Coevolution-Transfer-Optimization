c6e682f5e1c01230c4234d52dda61e10b7b8a738
erfan <mhi.erfan1@gmail.com>
2019-06-10 18:22:35 +0430

    Convex crossover implemented.

--- evolution/operators.py
+++ evolution/operators.py
@@ -1,8 +1,12 @@
+import numpy as np
 from abc import ABC, abstractmethod
+from evolution.chromosome import Chromosome
 
+def crossover(p1, p2, alpha=np.random.rand(), ctype='convex'):
+  print('alpha: ', alpha)
+  if ctype=='convex':
+    return Chromosome(alpha*p1.genes+(1-alpha)*p2.genes)
 
-def crossover():
-  pass
 
 def selection():
   pass
