8453383d6cf2125d026cee7f0d5d95e454418ccf
erfan <mhi.erfan1@gmail.com>
2019-06-10 18:22:03 +0430

    New initialization techniques added to individual.

--- evolution/individual.py
+++ evolution/individual.py
@@ -3,8 +3,13 @@ from abc import ABC, abstractmethod
 
 class Individual(ABC):
 
-  def __init__(self, n):
-    self.genes = np.zeros(n)
+  def __init__(self, n, init_func=np.random.rand):
+    if isinstance(n, int):
+      self.genes = init_func(n)
+    elif isinstance(n, np.ndarray):
+      self.genes = n
+    else:
+      raise ValueError('The input parameter n, is not valid')
 
   @abstractmethod
   def mutation(self):
