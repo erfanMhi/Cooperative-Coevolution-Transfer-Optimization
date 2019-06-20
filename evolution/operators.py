ef4f423fb3471991b96c1a7cea1f5ee124d999d1
Erfan <mhi.erfan1@gmail.com>
2019-06-20 14:36:45 +0430

    New operators added.

--- evolution/operators.py
+++ evolution/operators.py
@@ -1,6 +1,6 @@
 import numpy as np
 from abc import ABC, abstractmethod
-from evolution.chromosome import Chromosome
+from evolution.chromosome import Chromosome, AlphaChromosome
 
 def crossover(p1, p2, alpha=np.random.rand(), ctype='uniform'):
   print('alpha: ', alpha)
@@ -22,6 +22,21 @@ def total_crossover(pop):
   p2_selection = tmp < 0.5
   for i in range(psize):
     offsprings[i] = Chromosome(dims)
+    offsprings[i].genes[p1_selection[i]] = parent1[i].genes[p1_selection[i]]
+    offsprings[i].genes[p2_selection[i]] = parent2[i].genes[p2_selection[i]]
+  return offsprings
+
+def total_crossover_s2(pop):
+  dims = len(pop[0].genes)
+  psize = len(pop)
+  parent1 = pop[np.random.permutation(psize)]
+  parent2 = pop[np.random.permutation(psize)]
+  offsprings = np.ndarray(psize, dtype=object)
+  tmp = np.random.rand(psize, dims)
+  p1_selection = tmp >= 0.5
+  p2_selection = tmp < 0.5
+  for i in range(psize):
+    offsprings[i] = AlphaChromosome(dims)
     offsprings[i].genes[p1_selection[i]] = parent1[i].genes[p1_selection[i]]
     offsprings[i].genes[p2_selection[i]] = parent2[i].genes[p2_selection[i]]
   return offsprings
@@ -58,3 +73,15 @@ def get_pop_init(n, gn, init_func=np.random.rand):
   """
   return np.array([Chromosome(gn, init_func) for _ in range(n)])
 
+def get_pop_init_s2(n, gn, init_func=np.random.rand):
+  """[Initial Population Generator]
+  
+  Arguments:
+      n {[int]} -- [Number of members of population]
+      gn {[int]} -- [Number of individual's genes]
+      init_func {[function]} -- the function which initialize each chromosome
+  
+  Returns:
+      [np.ndarray] -- [Array of chromosomes]
+  """
+  return np.array([AlphaChromosome(gn, init_func) for _ in range(n)])
